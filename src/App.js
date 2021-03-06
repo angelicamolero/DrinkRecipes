8fb663c14bb2c97f0abf2c16359f1f45bbca3dc4
angelicamolero <angelicamoleror8@gmail.com>
2021-03-06 16:32:01 -0300

    creating first components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Header from './components/Header';
+import Form from './components/Form';
 
 function App() {
   return (
-    
+    <Fragment>
+       <Header/>
+       <div className="container mt-5">
+          <div className="row">
+            <Form/>
+          </div>
+       </div>
+    </Fragment>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Header = () => {
-    return();
+    return(
+        <header className="bg-alert">
+            <h1>Get Drink's Recipe</h1>
+        </header>
+    );
 }
 
 export default Header;
