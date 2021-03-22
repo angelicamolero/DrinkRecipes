fc2858721aba44c3ba27895fa407c40e9e709e3a
angelicamolero <angelicamoleror8@gmail.com>
2021-03-22 10:58:44 -0300

    Adding modal and material ui

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,7 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
 import { ModalContext } from '../context/ModalContext';
 
+
+function getModalStyle() {
+    const top = 50 ;
+    const left = 50;
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+}));
+
 const Recipe = ({recipe}) => {
+
+    //config modal
+    const [ modalStyle ] = useState(getModalStyle);
 
     const { saveIdRecipe } = useContext(ModalContext);
 
--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -1,11 +1,27 @@
 import React, { createContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
+
 export const ModalContext = createContext();
 
 const ModalProvider = ({children}) => {
 
     const [ idrecipe, saveIdRecipe ] = useState(null);
+    const [ recipe, saveRecipe ] =  useState({});
+
+    //once we have a recipe call the api
+    useEffect(() => {
+        const getRecipe = async () => {
+            if(!idrecipe) return;
+
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idrecipe}`;
+
+            const result = await axios.get(url);
+            saveRecipe(result.data.drinks[0]);
+        }
+        getRecipe();
+
+    }, [idrecipe])
 
     return(
         <ModalContext.Provider
